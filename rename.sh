372fc017ab5cec2b47d4b092cd616860675ef32f
Josep Jaume Rey Peroy <josepjaume@codegram.com>
2020-11-21 21:07:50 +0000

    Improve rename.sh

--- rename.sh
+++ rename.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-find . -type f -name '.gitignore' -print0 | xargs -0 sed -i '' -e 's/phoenix_starter/awesome_project/g'
-find . -type f -regex '*.\(eex\|ex\|exs\|js\|md\)' -print0 | xargs -0 sed -i '' -e 's/phoenix_starter/awesome_project/g'
-find . -type f -regex '*.\(eex\|ex\|exs\|js\|md\)' -print0 | xargs -0 sed -i '' -e 's/PhoenixStarter/AwesomeProject/g'
+find . -type f -name '.gitignore' -exec sed -i 's/phoenix_starter/awesome_project/g' {} \;
+find . -type f -regex '.*\.\(eex\|ex\|exs\|leex\|md\)' -exec sed -i 's/phoenix_starter/awesome_project/g' {} \;
+find . -type f -regex '.*\.\(eex\|ex\|exs\|leex\|md\)' -exec sed -i 's/PhoenixStarter/AwesomeProject/g' {} \;
