7ee38c2fb1d5a51347159744eb5a7ff68c62d417
Josep Jaume Rey Peroy <josepjaume@codegram.com>
2020-11-22 18:34:53 +0000

    Fix stylelint rules

--- assets/stylelint.config.js
+++ assets/stylelint.config.js
@@ -7,7 +7,36 @@ module.exports = {
   plugins: ["stylelint-prettier"],
   rules: {
     "prettier/prettier": true,
-    "at-rule-no-unknown": null,
-    "selector-pseudo-element-no-unknown": true,
+    "at-rule-empty-line-before": [
+      "always",
+      {
+        except: [
+          "blockless-after-blockless",
+          "blockless-after-same-name-blockless",
+          "first-nested",
+        ],
+        ignore: ["after-comment", "inside-block"],
+        ignoreAtRules: ["apply", "screen", "font-face", "nest"],
+      },
+    ],
+    "at-rule-no-unknown": [
+      true,
+      {
+        ignoreAtRules: [
+          "tailwind",
+          "variants",
+          "responsive",
+          "apply",
+          "screen",
+        ],
+      },
+    ],
+    "property-no-unknown": [
+      true,
+      {
+        ignoreProperties: ["font-path"],
+      },
+    ],
+    "selector-nested-pattern": "^&",
   },
 }
