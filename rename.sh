31cdbea1d6134b2a1938736e1d60657cf88a5f79
Josep Jaume Rey Peroy <josepjaume@codegram.com>
2020-11-23 20:23:57 +0000

    Fix renaming

--- rename.sh
+++ rename.sh
@@ -3,3 +3,4 @@
 find . -type f -name '.gitignore' -exec sed -i 's/phoenix_starter/awesome_project/g' {} \;
 find . -type f -regex '.*\.\(eex\|ex\|exs\|leex\|md\)' -exec sed -i 's/phoenix_starter/awesome_project/g' {} \;
 find . -type f -regex '.*\.\(eex\|ex\|exs\|leex\|md\)' -exec sed -i 's/PhoenixStarter/AwesomeProject/g' {} \;
+rename  's/phoenix_starter/awesome_project/' **/*
