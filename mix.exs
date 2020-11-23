1c6ad79f231f5d847ebde64c81a3a07205c81166
Josep Jaume Rey Peroy <josepjaume@codegram.com>
2020-11-23 20:30:28 +0000

    Add mix test watcher

--- mix.exs
+++ mix.exs
@@ -47,7 +47,8 @@ defmodule PhoenixStarter.MixProject do
       {:gettext, "~> 0.11"},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:mix_test_watch, "~> 1.0", only: :dev, runtime: false}
     ]
   end
 
