814ecf3ca33e825e9940037baf28958733ca5518
Edgar Latorre <me@edgarlatorre.com.br>
2021-03-24 12:26:12 +0100

    Change porwerlevel zsh theme to avit

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,2 +1,3 @@
 ## setup and install oh-my-zsh
 sh -c "$(wget -O- https://github.com/deluan/zsh-in-docker/releases/download/v1.0.2/zsh-in-docker.sh)"
+sed -i -e "s/powerlevel10k\/powerlevel10k/avit/g" /root/.zshrc
