912278513b6da3f51f8d9929aa0f4e3dcb6b2e59
Josep Jaume Rey <josepjaume@gmail.com>
2021-09-01 16:28:57 +0200

    Fix seeds and readme

--- lib/phoenix_starter_utils.ex
+++ lib/phoenix_starter_utils.ex
@@ -1,2 +1,4 @@
 defmodule PhoenixStarterUtils do
+  def delete_data(are_you_sure: :yes) do
+  end
 end
--- rename.sh
+++ rename.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 find . -type f -name '.gitignore' -exec sed -i 's/phoenix_starter/awesome_project/g' {} \;
+find . -type f -name '.gitignore' -exec sed -i 's/codegram\/phoenix_starter/codegram\/awesome_project/g' {} \;
 find . -type f -regex '.*\.\(eex\|ex\|exs\|leex\|md|json|yml\)' -exec sed -i 's/phoenix_starter/awesome_project/g' {} \;
 find . -type f -regex '.*\.\(eex\|ex\|exs\|leex\|md|json|yml\)' -exec sed -i 's/PhoenixStarter/AwesomeProject/g' {} \;
 rename  's/phoenix_starter/awesome_project/' **/*
